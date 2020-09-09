e3e98c65a84e90048f79beb1cb94ae804f9f3bdb
ColceriuVlad <70012191+ColceriuVlad@users.noreply.github.com>
2020-09-09 17:01:02 +0300

    MineSweeper Commit

--- index.js
+++ index.js
@@ -14,36 +14,30 @@ let clients = new MongoClient(uri, {
     useUnifiedTopology: true
 })
 
+let minesweeperdb;
+
+clients.connect(error => {
+    if (error) {
+        console.error(error)
+    }
+    minesweeperdb = clients.db("minesweeperdb")
+})
 
 // query the database, sort it by clicks in an ascending manner and send it back to the client where it is logged into the Winner menu
 app.get("/winnerquery", function (request, response) {
-    clients.connect(error => {
+    minesweeperdb.collection("scores").find({}).sort({
+        clicks: 1
+    }).toArray(function (error, element) {
         if (error) {
             console.error(error)
         }
-        minesweeperdb = clients.db("minesweeperdb")
-        minesweeperdb.collection("scores").find({}).sort({
-            clicks: 1
-        }).toArray(function (error, element) {
-            if (error) {
-                console.error(error)
-            }
-            response.json(element)
-        })
-
+        response.json(element)
     })
 })
 
 
 //recieves the current winner from client side, adds it to the database and sends it back to be included in the Winner section
 app.post("/winner", function (request, response) {
-    clients.connect(error => {
-        if (error) {
-            console.error(error)
-        }
-        minesweeperdb = clients.db("minesweeperdb")
-        minesweeperdb.collection("scores").insertOne(request.body)
-    })
+    minesweeperdb.collection("scores").insertOne(request.body)
     response.json(request.body)
-
 })
